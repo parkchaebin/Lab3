49cbf34e2147c30b622b833d39012746078d4e27
parkchaebin <balbin0306@naver.com>
2022-11-29 02:07:06 +0900

    Q5 완료

---
+++ Q5/exitprocess.c
@@ -0,0 +1,12 @@
+/* exitprocess.c */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int exit_status;
+	printf("enter exit status: ");
+	scanf("%d", &exit_status);
+	exit(exit_status);
+}
